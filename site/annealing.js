d5682f7bfb41a5cc65f63583b9359b8f57a5b238
Thomas H <tom@mathpunk.net>
2016-03-31 17:18:39 -0700

    Use view templating (handlebars)
    ...instead of raw creation of the content inside the route functions
    themselves.

--- site/annealing.js
+++ site/annealing.js
@@ -1,48 +1,38 @@
+// Bring in modules
 var express = require('express');
-
 var app = express();
-
 var handlebars = require('express-handlebars').create(
     { defaultLayout: 'main' }
 ); // common, I bet: symbol key, string value
 
+// Set app parameters
 app.engine('handlebars', handlebars.engine); // wtf?
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
 
+// Define routes
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Annealing is an app');
-});
-app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Annealing is an app');
+    res.render('home');
 });
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('The theory of annealing');
+    res.render('about');
     // The content of this page, if you decide to have it, should be an edited version of the /readme.org/ page you wrote like a monkey at a typewriter.
 });
 
-// 404 : Page not found
-app.use(function (req, res) {
-    res.type('text/plain');
+// 404 handler
+app.use(function (req, res, next){
     res.status(404);
-    res.send('404 - Not Found')
-})
+    res.render('404');   // as a number, then as a string. that's slightly silly
+});
 
-// 500 : Server error
-app.use(function(err, req, res, next){
-    console.error(err.stack); // what? I guess we expect the err object to have this attr
-    res.type('text/plain');
+// 500 handler
+app.use(function (req, res, next) {
+    console.error(err.stack);
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');   // as a number, then as a string. that's a little silly
 });
 
 // Go ahead, I'm listening
 app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:'+
-                app.get('port')+
-                '; press Ctrl-C to terminate.');
+    console.log('Express started on http://localhost:'+ app.get('port')+ '; press Ctrl-C to terminate.');
 });
